c8c89dabe73ea236f07e39262fd47c6977a59fbd
Paul Sydney Orozco <xtrycatchx@gmail.com>
2017-01-01 18:29:48 +0800

    updated port to be compat with heroku env

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var express = require('express');
 
 var app = express();
 
+app.set('port', (process.env.PORT || 3000));
+
 var staticPath = path.join(__dirname, '/public');
 app.use(express.static(staticPath));
 
@@ -20,6 +22,6 @@ app.post('/', function(req, res) {
     //TODO: add processing for submitted blood inquiry
 });
 
-app.listen(3000, function() {
+app.listen(app.get('port'), function() {
   console.log('listening');
 });
