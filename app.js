dc1207c589bbdc8237f71b4b601c6fd2b476066b
Paul Sydney Orozco <xtrycatchx@gmail.com>
2017-01-02 10:35:03 +0800

    exposed APIs for unit tests

--- app.js
+++ app.js
@@ -6,10 +6,14 @@
 
 var path = require('path');
 var express = require('express');
+var helmet = require('helmet');
+var server;
 
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
+
+app.use(helmet());
 
 var staticPath = path.join(__dirname, '/public');
 app.use(express.static(staticPath));
@@ -22,6 +26,18 @@ app.post('/', function(req, res) {
     //TODO: add processing for submitted blood inquiry
 });
 
-app.listen(app.get('port'), function() {
-  console.log('listening');
-});
+var start = function () {
+    server = app.listen(app.get('port'), function() {
+        console.log('listening on port: ', app.get('port'));
+    });
+}
+
+var shutdown = function() {
+    console.log('shutting down app');
+    server.close();
+}
+
+exports.start = start;
+exports.shutdown = shutdown;
+exports.port = app.get('port');
+
