63c8b7bb8e3d4f0a2ec9ca31ac6dcf685366cab7
Paul Sydney Orozco <xtrycatchx@gmail.com>
2017-01-04 01:04:29 +0800

    added post handler to /inquire

--- app.js
+++ app.js
@@ -7,6 +7,7 @@
 var path = require('path');
 var express = require('express');
 var helmet = require('helmet');
+var bodyParser = require('body-parser');
 var server;
 
 var app = express();
@@ -18,12 +19,22 @@ app.use(helmet());
 var staticPath = path.join(__dirname, '/public');
 app.use(express.static(staticPath));
 
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({	extended: true })); // support encoded bodies
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.post('/', function(req, res) {
+app.post('/inquire', function(req, res) {
     //TODO: add processing for submitted blood inquiry
+    var fullName = req.body.fullName;
+	var emailAdd = req.body.emailAddress;
+	var contactN = req.body.contactNumber;
+    var bloodTpe = req.body.bloodType;
+    console.log('broadcast this info to trusted blood providers: ', fullName, emailAdd, contactN, bloodTpe);
+    res.send(fullName + ' ' + emailAdd + ' ' + contactN + ' ' + bloodTpe);
 });
 
 var start = function () {
