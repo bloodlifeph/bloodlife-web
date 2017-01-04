20d55dc6166857ecdee33ab4112a674c594b51f7
Paul Sydney Orozco <xtrycatchx@gmail.com>
2017-01-04 18:32:37 +0800

    cleaned-up client side post helper

--- public/js/submit.js
+++ public/js/submit.js
@@ -1,18 +1,14 @@
 $(document).ready(function() {
-
 	// process the form
 	$('#inquireForm').submit(function(event) {
-
 		$('.form-group').removeClass('has-error'); // remove the error class
 		$('.help-block').remove(); // remove the error text
-
-		// get the form data
-		// there are many ways to get this data using jQuery (you can use the class or id also)
+		
 		var formData = {
-			'fullName' 				: $('input[name=fullName]').val(),
-			'emailAddress' 			: $('input[name=emailAddress]').val(),
-			'contactNumber' 	: $('input[name=contactNumber]').val(),
-            'bloodType' 	: $('input[name=bloodType]').val(),
+			'fullName' : $('#fullName').val(),
+			'emailAddress' : $('#emailAddress').val(),
+			'contactNumber' : $('#contactNumber').val(),
+            'bloodType' : $('#bloodType').val()
 		};
 
 		// process the form
@@ -23,56 +19,47 @@ $(document).ready(function() {
 			dataType 	: 'json', // what type of data do we expect back from the server
 			encode 		: true
 		})
-			// using the done promise callback
-			.done(function(data) {
-
-				// log data to the console so we can see
-				console.log(data); 
-
-                
-
-				// here we will handle errors and validation messages
-				if ( ! data.success) {
-					
-					// handle errors for name ---------------
-					if (data.errors.fullName) {
-						$('#name-group').addClass('has-error'); // add the error class to show red input
-						$('#name-group').append('<div class="help-block">' + data.errors.fullName + '</div>'); // add the actual error message under our input
-					}
-
-					// handle errors for emailAddress ---------------
-					if (data.errors.emailAddress) {
-						$('#email-group').addClass('has-error'); // add the error class to show red input
-						$('#email-group').append('<div class="help-block">' + data.errors.emailAddress + '</div>'); // add the actual error message under our input
-					}
-
-					// handle errors for contactNumber ---------------
-					if (data.errors.contactNumber) {
-						$('#superhero-group').addClass('has-error'); // add the error class to show red input
-						$('#superhero-group').append('<div class="help-block">' + data.errors.contactNumber + '</div>'); // add the actual error message under our input
-					}
-
-				} else {
-
-					// ALL GOOD! just show the success message!
-					$('#inquireForm').append('<div class="alert alert-success">' + data.message + '</div>');
-
-					// usually after form submission, you'll want to redirect
-					// window.location = '/thank-you'; // redirect a user to another page
-
+		
+		// using the done promise callback
+		.done(function(data) {
+			// log data to the console so we can see
+			console.log(data);
+			
+			// here we will handle errors and validation messages
+			if ( data.result === 'NOK') {
+				
+				// handle errors for name ---------------
+				if (data.errors.fullName) {
+					$('#fullName-group').addClass('has-error'); // add the error class to show red input
+					$('#fullName-group').append('<div class="help-block">' + data.errors.fullName + '</div>'); // add the actual error message under our input
 				}
-			})
+				
+				// handle errors for emailAddress ---------------
+				if (data.errors.emailAddress) {
+					$('#emailAddress-group').addClass('has-error'); // add the error class to show red input
+					$('#emailAddress-group').append('<div class="help-block">' + data.errors.emailAddress + '</div>'); // add the actual error message under our input
+				}
+				
+				// handle errors for contactNumber ---------------
+				if (data.errors.contactNumber) {
+					$('#contactNumber-group').addClass('has-error'); // add the error class to show red input
+					$('#contactNumber-group').append('<div class="help-block">' + data.errors.contactNumber + '</div>'); // add the actual error message under our input
+				}
 
-			// using the fail promise callback
-			.fail(function(data) {
-
-                // show any errors
-				// best to remove for production
-				console.log(data);
-			});
+			} else {
+				// show success message!
+				$('#inquireForm').append('<div class="alert alert-success">' + data.message + '</div>');
+			}
+		})
+		// using the fail promise callback
+		.fail(function(data) {
+			$('#inquireForm').append('<div class="alert alert-success">' + JSON.stringify(data) + '</div>');
+			// show any errors
+			// best to remove for production
+			console.log(data);
+		});
 
 		// stop the form from submitting the normal way and refreshing the page
 		event.preventDefault();
 	});
-
 });
